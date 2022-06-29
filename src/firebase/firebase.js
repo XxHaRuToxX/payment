2b20434e25994d6532871275a870073c0acf1815
XxHaRuToxX <youlayelvis@gmail.com>
2022-06-28 22:15:23 -0500

    firebase updated

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,7 +3,17 @@ import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
 
-  // tu configuracion de firebase
+  apiKey: "AIzaSyCBy113COseUuPy-NJQAGLd_eL1wlJ_Ico",
+
+  authDomain: "payment-rpa.firebaseapp.com",
+
+  projectId: "payment-rpa",
+
+  storageBucket: "payment-rpa.appspot.com",
+
+  messagingSenderId: "4050551087",
+
+  appId: "1:4050551087:web:1613fea9cadcbaa474a1da"
 
 };
 
